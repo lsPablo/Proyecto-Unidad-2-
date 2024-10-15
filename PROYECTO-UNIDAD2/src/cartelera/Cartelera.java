64b19e13afb38cb14e42dd7811e0587f01719775
Victor Antonio Hernandez Paredes <victor5321@outlook.com>
2024-10-15 08:28:22 -0600

    atributos clase cartelera

--- PROYECTO-UNIDAD2/src/cartelera/Cartelera.java
+++ PROYECTO-UNIDAD2/src/cartelera/Cartelera.java
@@ -1,5 +1,42 @@
 package cartelera;
 
-public class Cartelera {
+import peliculas.Pelicula;
+import salas.Sala;
 
+import java.time.LocalTime;
+
+public class Cartelera {
+    public Pelicula pelicula;
+    public Sala sala;
+    public LocalTime horaProyeccion;
+
+    public Cartelera(Pelicula pelicula, Sala sala, LocalTime horaProyeccion) {
+        this.pelicula = pelicula;
+        this.sala = sala;
+        this.horaProyeccion = horaProyeccion;
+    }
+
+    public Pelicula getPelicula() {
+        return pelicula;
+    }
+
+    public Sala getSala() {
+        return sala;
+    }
+
+    public LocalTime getHoraProyeccion() {
+        return horaProyeccion;
+    }
+
+    public void setPelicula(Pelicula pelicula) {
+        this.pelicula = pelicula;
+    }
+
+    public void setSala(Sala sala) {
+        this.sala = sala;
+    }
+
+    public void setHoraProyeccion(LocalTime horaProyeccion) {
+        this.horaProyeccion = horaProyeccion;
+    }
 }
