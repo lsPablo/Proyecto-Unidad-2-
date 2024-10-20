9b20c908fcbe586f1073555e7f86a50100d9d876
Victor Antonio Hernandez Paredes <victor5321@outlook.com>
2024-10-19 22:17:17 -0600

    si borro algo perdon

--- PROYECTO-UNIDAD2/src/pago/tarjeta/Tarjeta.java
+++ PROYECTO-UNIDAD2/src/pago/tarjeta/Tarjeta.java
@@ -6,19 +6,6 @@ import usuarios.cliente.Cliente;
 
 import java.time.LocalDateTime;
 
-public class Tarjeta extends ImprimirBoleto {
+public class Tarjeta {
 
-    public String tarjeta;
-
-    public Tarjeta(String id, Cliente cliente, Producto producto, LocalDateTime fechaCompra, String tarjeta) {
-        super(id, cliente, producto, fechaCompra);
-        this.tarjeta = tarjeta;
-    }
-
-    public String getTarjeta() {
-        return tarjeta;
-    }
-   // public String pagar(){
-       // return String.format("")
-    ///}
 }
